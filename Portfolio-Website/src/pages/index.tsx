ecafc986b18618010495d68d9542af836858302c
rajc1729gitcommitexitgit config --global user.name rajc1729 <chhatbarraj@csu.fullerton.edu>
2020-11-23 09:43:10 -0800

    :pencil: Structure Setup

---
+++ Portfolio-Website/src/pages/index.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+import Layout from "../components/layout"
+import "../styles/index.scss"
+
+
+const IndexPage = () => (
+  <>
+  <Layout>
+    <section id="landing">Hero section</section>
+    <section id="about">About section</section>
+    <section id="work">Work section</section>
+    <section id="contact">contact section</section>
+  </Layout>
+  </>
+)
+
+export default IndexPage
