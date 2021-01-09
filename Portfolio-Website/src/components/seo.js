21888d30b62c82895f41fa00887769fa311ba8a4
rajc1729gitcommitexitgit config --global user.name rajc1729 <chhatbarraj@csu.fullerton.edu>
2021-01-09 09:41:11 -0800

    :zap: UI Update

--- Portfolio-Website/src/components/seo.js
+++ Portfolio-Website/src/components/seo.js
@@ -49,7 +49,7 @@ function SEO({ description, lang, meta, title }) {
         },
         {
           property: `og:title`,
-          content: `${site.siteMetadata.title} | ${title}`,
+          content: `${site.siteMetadata.title} | %s`,
         },
         {
           property: `og:description`,
