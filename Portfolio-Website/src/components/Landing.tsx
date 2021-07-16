6fe346f87209210a615627a911813c88e5313ede
rajc1729gitcommitexitgit config --global user.name rajc1729 <chhatbarraj@csu.fullerton.edu>
2021-07-16 10:04:13 -0700

    :zap: Resume

--- Portfolio-Website/src/components/Landing.tsx
+++ Portfolio-Website/src/components/Landing.tsx
@@ -1,50 +1,45 @@
 import React from "react"
 import "../styles/Landing.scss"
-import raj from "../images/Raj_Chhatbar.png";
+import raj from "../images/Raj_Chhatbar.png"
 import Data from "../assets/data"
 
 import Img from "gatsby-image"
 
-const Landing = ({pic}) => {
-    return (
-        <>
-            <section id="landing">
-                <div className="landing_container">
-                
-                <div className="intro_container">
-                    <span >👋 Hello!  my name is</span>
-                    <h1>{Data.name}.</h1>
-                    <h4>{Data.title}</h4>
-                    <p>
-                        {Data.heroDescription}
-                    </p>
+const Landing = ({ pic }) => {
+  return (
+    <>
+      <section id="landing">
+        <div className="landing_container">
+          <div className="intro_container">
+            <span>👋 Hello! my name is</span>
+            <h1>{Data.name}.</h1>
+            <h4>{Data.title}</h4>
+            <p>{Data.heroDescription}</p>
 
-                    <div className="button_container">
-                        <a 
-                        title="resume"
-                        href={"/static/Raj_resume_v12.pdf"}
-                        target="_blank"
-                        rel="nofollow noopener noreferrer"
-                        className="dark_button">
-                            Resume
-                        </a>
+            <div className="button_container">
+              <a
+                title="resume"
+                href={"/static/Raj_resume_v13.pdf"}
+                target="_blank"
+                rel="nofollow noopener noreferrer"
+                className="dark_button"
+              >
+                Resume
+              </a>
+            </div>
+          </div>
 
-                        {/* <button className="light_button">
-                            Resume
-                        </button> */}
-                    </div>
-                </div>
-
-
-
-
-                <div className="imageContainer">
-                    <Img fluid={pic.childImageSharp.fluid} alt="Avatar" className="avatar_img"></Img>
-                </div>
-                </div>
-            </section>
-        </>
-    )
+          <div className="imageContainer">
+            <Img
+              fluid={pic.childImageSharp.fluid}
+              alt="Avatar"
+              className="avatar_img"
+            ></Img>
+          </div>
+        </div>
+      </section>
+    </>
+  )
 }
 
 export default Landing
