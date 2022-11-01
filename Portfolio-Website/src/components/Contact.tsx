2ef02cb3ac54346a70a440e1d901a02b3b1aab3d
Raj Chhatbar <rajchh@amazon.com>
2022-11-01 22:15:19 +0530

    :zap: Update contact

--- Portfolio-Website/src/components/Contact.tsx
+++ Portfolio-Website/src/components/Contact.tsx
@@ -10,7 +10,9 @@ const Contact = () => {
           <span className="contact_title">Get In Touch</span>
 
           <p className="contact_description">
-            I'm not looking for any new opportunities
+            Although I’m not currently looking for any new opportunities, my
+            inbox is always open. Whether you have a question or just want to
+            say hi, I’ll try my best to get back to you!
           </p>
 
           <a
